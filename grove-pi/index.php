247a1bcdc45b08ba072dfcf1bc964b6b0174b3b0
Peter 4A5A <498883@edu.rocmn.nl>
2017-11-14 14:42:46 +0100

    Fixed the script

--- grove-pi/index.php
+++ grove-pi/index.php
@@ -1,5 +1,5 @@
 <?php
-  if (ISSET($_POST['piID'])) {
+  if (ISSET($_REQUEST['piID'])) {
     require_once 'config.php';
     require_once 'model/databaseHandler.class.php';
     require_once 'model/Security.class.php';
@@ -7,29 +7,18 @@
     $DatabaseHandler = new db();
     $Security = new Security();
 
-    $sql = "INSERT INTO `measure` (
-      `piID`,
-      `measureType`,
-      `measureValue`,
-      `time`,
-      `date`)
-      VALUES (
-      :piID,
-      :measureType,
-      :measureValue,
-      :time,
-      :date,
-      )";
+    $sql = "INSERT INTO `measure` (`piID`,`measureType`,`measureValue`,`tijd`,`datum`) VALUES (:piID, :measureType, :measureValue, :tijd, :datum)";
 
       $input = array(
-        "piID" => $Security->checkInput($_POST['piID']),
-        "measureType" => $Security->checkInput($_POST['measureType']),
-        "measureValue" => $Security->checkInput($_POST['measureValue']),
-        "time" => date("H:i:s"),
-        "date" => date("d-m-Y"),
+        "piID" => $Security->checkInput($_REQUEST['piID']),
+        "measureType" => $Security->checkInput($_REQUEST['measureType']),
+        "measureValue" => $Security->checkInput($_REQUEST['measureValue']),
+        "tijd" => date("H:i:s"),
+        "datum" => date("d-m-Y"),
       );
 
       $result = $DatabaseHandler->CreateData($sql, $input);
+      var_dump($result);
   }
 
   else {
